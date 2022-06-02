6a0b0e3e30a66dbe6406bb9695f2a8696284a900
lukevoss <luke@familievoss.net>
2022-06-02 16:28:05 +0200

    Merged Home status

---
+++ BaysianLSTM.py
@@ -0,0 +1,13 @@
+# My first try to create a Bayesian LSTM Network
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+lstm = nn.LSTM(3, 3)  # Input dim is 3, output dim is 3
+inputs = [torch.randn(1, 3) for _ in range(5)]  # make a sequence of length 5
+inputs = torch.cat(inputs).view(len(inputs), 1, -1)
+hidden = (torch.randn(1, 1, 3), torch.randn(1, 1, 3))  # clean out hidden state
+out, hidden = lstm(inputs, hidden)
