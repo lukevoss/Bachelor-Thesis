8f89d8a065c65c0ace50cf4c42bd149cf1be01ae
Luke Voss <luke@familievoss.net>
2022-07-13 10:24:22 +0200

    Trying settings

--- BayesianLSTMs/Pytorch_airpassengers.py
+++ BayesianLSTMs/Pytorch_airpassengers.py
@@ -27,8 +27,8 @@ def sliding_windows(data, seq_length):
         _y = data[i+seq_length]
         x.append(_x)
         y.append(_y)
-    x = torch.tensor(x).float()
-    y = torch.tensor(y).float()
+    x = torch.tensor(x).float() #Solved: Hier lag Fehler
+    y = torch.tensor(y).float() #Solved: Hier lag Fehler
     return x,y
 
 #preprocessing data
@@ -46,7 +46,7 @@ trainX, testX, trainY, testY = train_test_split(x,
                                                     y,
                                                     test_size=.3,
                                                     random_state=42,
-                                                    shuffle=False)
+                                                    shuffle=False)#Solved: Hier lag Fehler
 # dataX = Variable(torch.Tensor(np.array(x)))
 # dataY = Variable(torch.Tensor(np.array(y)))
 
@@ -61,7 +61,7 @@ train_months = np.arange(start=0,stop=train_size,step=1)
 test_months = np.arange(start=train_size, stop=train_size+test_size, step=1)
 
 ds = torch.utils.data.TensorDataset(trainX, trainY)
-dataloader_train = torch.utils.data.DataLoader(ds, batch_size=8, shuffle=True)
+dataloader_train = torch.utils.data.DataLoader(ds, batch_size=20, shuffle=True)
 
 #build model
 @variational_estimator           
@@ -94,11 +94,11 @@ class BayesianNN(nn.Module):
         return out
 
 # Training
-num_epochs = 1000
+num_epochs = 1500
 learning_rate = 0.001
 
 input_size = 1
-hidden_size = 10
+hidden_size = 2
 num_layers = 1
 
 num_classes = 1
@@ -118,7 +118,7 @@ for epoch in range(num_epochs):
         loss = bayesian_network.sample_elbo(inputs=datapoints,
                             labels=labels,
                             criterion=criterion, 
-                            sample_nbr=3,
+                            sample_nbr=7,
                             complexity_cost_weight=1/x.shape[0])
         loss.backward()
         
@@ -139,30 +139,30 @@ plt.plot(dataY_plot, linestyle = 'dashed', color = 'black', linewidth = 3, label
 plt.scatter(months,dataY_plot, s = 30, alpha = 1, marker = "o", color = 'red', label = 'Data')
 plt.axvline(x=train_size, c='r', linestyle='--')
 
-ensemble_size = 5
+ensemble_size = 20
 
-# test_predict = [bayesian_network(testX) for i in range(ensemble_size)]
-# for i in range(ensemble_size):
-#     prediction = test_predict[i] 
-#     prediction= prediction.data.numpy()
-#     prediction = sc.inverse_transform(prediction)
-#     test_predict[i] = prediction
-# test_predict_mean =np.stack(test_predict)
-# means = test_predict_mean.mean(axis=0)
-# stds = test_predict_mean.std(axis=0)
-# y_upper = means + (2 * stds)
-# y_lower = means - (2 * stds)
+test_predict = [bayesian_network(testX) for i in range(ensemble_size)]
 for i in range(ensemble_size):
-    prediction = bayesian_network(testX)
-    #prediction = prediction[:, 0, :]
+    prediction = test_predict[i] 
     prediction= prediction.data.numpy()
     prediction = sc.inverse_transform(prediction)
-    plt.plot(test_months, prediction, color= 'cornflowerblue', alpha=0.8, linewidth = 3, label='Learned Model')
-#plt.fill_between(test_months, y_upper[:,0], y_lower[:,0], alpha = 0.4, color='skyblue', label='Standard Deviation')
+    test_predict[i] = prediction
+test_predict_mean =np.stack(test_predict)
+means = test_predict_mean.mean(axis=0)
+stds = test_predict_mean.std(axis=0)
+y_upper = means + (2 * stds)
+y_lower = means - (2 * stds)
+# for i in range(ensemble_size):
+#     prediction = bayesian_network(testX)
+#     #prediction = prediction[:, 0, :]
+#     prediction= prediction.data.numpy()
+#     prediction = sc.inverse_transform(prediction)
+#     plt.plot(test_months, prediction, color= 'cornflowerblue', alpha=0.8, linewidth = 3, label='Learned Model')
+plt.fill_between(test_months, y_upper[:,0], y_lower[:,0], alpha = 0.4, color='skyblue', label='Standard Deviation')
 
 
 
-#plt.plot(test_months,data_predict[:,0], label = 'Prediction')
+plt.plot(test_months,means, label = 'Prediction')
 plt.suptitle('Time-Series Prediction')
 plt.legend()
 plt.show()
